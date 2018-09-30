38eddac67e042ca3e62211be687479294620ecf1
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-30 18:15:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1440,128 +1440,6 @@ var mentionned = message.mentions.members.first();
 
 
 
- client.on("roleCreate", rc => {
-  const channel = rc.guild.channels.find("name", "log") //تقدر تغير اسم الشات
-  if(channel) {
-  var embed = new Discord.RichEmbed()
-  .setTitle(rc.guild.name)
-  .setDescription(`***Created Role Name : *** **${rc.name}** `)
-  .setColor(`RANDOM`)
-  .setTimestamp(); 
-  channel.sendEmbed(embed)
-  }
-  });
-  //By S Codes
-  client.on("roleDelete",  rd => {
-  const channel = rd.guild.channels.find("name", "log")
-  if(channel) {
-  var embed = new Discord.RichEmbed()
-  .setTitle(rd.guild.name)
-  .setDescription(`***Deleted Role Name : *** **${rd.name}** `)
-  .setColor(`RANDOM`)
-  .setTimestamp(); 
-  channel.sendEmbed(embed)
-  }
-  });
-
-client.on("channelCreate",  cc => {
-  const channel = cc.guild.channels.find("name", "log")
-  if(channel) {
-  var embed = new Discord.RichEmbed()
-  .setTitle(cc.guild.name)
-  .setDescription(`***Channel Created Name : *** **${cc.name}** ⬅️`)
-  .setColor(`RANDOM`)
-  .setTimestamp(); 
-  channel.sendEmbed(embed)
-  }
-  });
-
-   client.on("deleteChannel",  dc => {
-  const channel = dc.guild.channels.find("name", "log")
-  if(channel) {
-  var embed = new Discord.RichEmbed()
-  .setTitle(dc.guild.name)
-  .setDescription(`***Channel Deleted Name : *** **${dc.name}** ⬅️`)
-  .setColor(`RANDOM`)
-  .setTimestamp(); 
-  channel.sendEmbed(embed)
-  }
-  });
-  
-  
-  
-  client.on('messageUpdate', (message, newMessage) => {
-    if (message.content === newMessage.content) return;
-    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
-    const channel = message.guild.channels.find('name', 'log');
-    if (!channel) return;
-
-    let embed = new Discord.RichEmbed()
-       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-       .setColor('SILVER')
-       .setDescription(`✏ **تعديل رساله
-ارسلها <@${message.author.id}>                                                                                                                         تم تعديلها في شات** <#${message.channel.id}>\n\nقبل التعديل:\n \`${message.cleanContent}\`\n\nبعد التعديل:\n \`${newMessage.cleanContent}\``)
-       .setTimestamp();
-     channel.send({embed:embed});
-
-
-});
-
-client.on('guildMemberAdd', member => {
-    if (!member || !member.id || !member.guild) return;
-    const guild = member.guild;
-	
-    const channel = member.guild.channels.find('name', 'log');
-    if (!channel) return;
-    let memberavatar = member.user.avatarURL
-    const fromNow = moment(member.user.createdTimestamp).fromNow();
-    const isNew = (new Date() - member.user.createdTimestamp) < 900000 ? '🆕' : '';
-    
-    let embed = new Discord.RichEmbed()
-       .setAuthor(`${member.user.tag}`, member.user.avatarURL)
-	   .setThumbnail(memberavatar)
-       .setColor('GREEN')
-       .setDescription(`📥 <@${member.user.id}> **Joined To The Server**\n\n`)
-       .setTimestamp();
-     channel.send({embed:embed});
-});
-
-client.on('guildMemberRemove', member => {
-    if (!member || !member.id || !member.guild) return;
-    const guild = member.guild;
-	
-    const channel = member.guild.channels.find('name', 'log');
-    if (!channel) return;
-    let memberavatar = member.user.avatarURL
-    const fromNow = moment(member.joinedTimestamp).fromNow();
-    
-    let embed = new Discord.RichEmbed()
-       .setAuthor(`${member.user.tag}`, member.user.avatarURL)
-	   .setThumbnail(memberavatar)
-       .setColor('RED')
-       .setDescription(`📤 <@${member.user.id}> **Leave From Server**\n\n`)
-       .setTimestamp();
-     channel.send({embed:embed});
-});
-
-client.on('messageDelete', message => {
-    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
-    const channel = message.guild.channels.find('name', 'log');
-    if (!channel) return;
-    
-    let embed = new Discord.RichEmbed()
-       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-       .setColor('BLACK')
-       .setDescription(`🗑️ **حذف رساله**
-**ارسلها <@${message.author.id}>                                                                                                                        تم حذفها في شات** <#${message.channel.id}>\n\n \`${message.cleanContent}\``)
-       .setTimestamp();
-     channel.send({embed:embed});
-
-});
-
-
-
-
 
 
 
@@ -1569,9 +1447,121 @@ client.on('messageDelete', message => {
 
 
   
-
-
-
+const fs = require('fs');
+var Swears = JSON.parse(fs.readFileSync("./swears.json", "utf8"));
+client.on('message', message => {
+    var args = message.content.toLowerCase().split(' ');
+    var args1 = args.slice(1).join(' ');
+    var command = args[0];
+    var prefix = '+'; // <====== تقدر تغير البرفكس
+   
+   
+    if(Swears.some(word => message.content.toLowerCase().includes(word))) {
+        if(message.member.hasPermission('ADMINISTRATOR')) return;
+        message.delete();
+        message.channel.send(`:no_entry: | Hey <@${message.author.id}>! Dont swear or you will get mute!`).then(msg => msg.delete(2000));
+    }
+   
+   
+    if(command == prefix + 'rdod') {// حقوق الفا كودز & عبود
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(':no_entry: | You dont have **MANAGE_MESSAGES** Permission!');
+        if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return meessage.channel.send(':no_entry: | I dont have **EMBED_LINKS** Permission!');
+        if(Swears.length < 1) return message.channel.send(`:no_entry: | No swears words founds! \`\`If you want to add some words type ${prefix}add-swear <SWEAR>\`\``);
+        var number = 1;// حقوق الفا كودز & عبود
+       
+        if(!args[1] || isNaN(args[1]) || args[1] === '1') {// حقوق الفا كودز & عبود
+            if(Swears.length > 10) {
+                var more = `\n__:sparkles: **More?** \`\`${prefix}swears 2\`\` (${Math.round(Math.round(Swears.length / 10) + 1)} pages)`;
+            }else {
+                var more = '\n__';
+            }
+           
+            let swearsWords = new Discord.RichEmbed()// حقوق الفا كودز & عبود
+            .setTitle(`:white_check_mark: **${Swears.length}** Swears Words.`)
+            .setColor('RED')
+            .setDescription(`__\n__${Swears.map(w => `**${number++}.** ${w}`).slice(0, 10).join('\n')}__\n${more}`)
+            .setTimestamp()
+            .setFooter(message.author.tag, message.author.avatarURL)
+           
+            message.channel.send(swearsWords);
+        }else if(!isNaN(args[1])) {// حقوق الفا كودز & عبود
+            if(Swears.length < 10) {
+                var morepage = 'This server have **1** Pages only.';
+            }else {
+                var morepage = `Please select page from 1 to ${Math.round(Swears.length / 10) + 1}`;
+            }
+            if(args[1] > Math.round(Swears.length / 10) + 1) return message.channel.send(`:no_entry: | I couldn\'t find the page. ${morepage}`);
+           if(args[1] < 1) return message.channel.send(`:no_entry: | I couldn\'t find the page. ${morepage}`);// حقوق الفا كودز & عبود
+           if(Swears.length > 10) {
+               var more = `\n__:sparkles: **More?** \`\`${prefix}swears ${Math.round(args[1]) + 1}\`\` (${Math.round(Swears.length / 10) + 1} pages)`;
+           }else {
+               var more = '\n__';
+           }
+           if(args[1] === '5' && Swears.length > 40) more = '\n__';// حقوق الفا كودز & عبود
+           var number = 1;
+         
+           let swearsWords = new Discord.RichEmbed()
+           .setTitle(`:white_check_mark: **${Swears.length}** Swears Words.`)
+           .setColor('RED')
+           .setDescription(`__\n__${Swears.map(w => `**${number++}.** ${w}`).slice(10 * Math.round(args[1].replace('-', '')) - 10, 10 * Math.round(args[1].replace('-', ''))).join('\n')}__\n${more}`)
+           .setTimestamp()
+           .setFooter(message.author.tag, message.author.avatarURL)// حقوق الفا كودز & عبود
+         
+           message.channel.send(swearsWords);
+       }
+   }
+ 
+ 
+   if(command == prefix + 'add-rd') {// حقوق الفا كودز & عبود
+       if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(':no_entry: | You dont have **ADMINISTRATOR** Permission!');
+       if(!args1) return message.channel.send(`**➥ Useage:** ${prefix}add-swear <SWEAR>`);
+       if(Swears.length == 50) return message.channel.send(':no_entry: | Maxmium number of swears is **50**');
+       if(args1.length > 30) return message.channel.send(`:no_entry: | The swear **${args1.length}** characters. Please try with characters less then **30**`);
+     
+       Swears.push(args1);// حقوق الفا كودز & عبود
+       fs.writeFile('./swears.json', JSON.stringify(Swears), (err) => {
+           if(err) console.error(err);
+       })
+       message.channel.send(`:white_check_mark: | Successfully added **${args1}** To swears words!`);
+   }// حقوق الفا كودز & عبود
+ 
+ 
+   if(command == prefix + 'remove-swear') {// حقوق الفا كودز & عبود
+       if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(':no_entry: | You dont have **ADMINISTRATOR** Permission!');
+       if(Swears.length < 1) return message.channel.send(`:no_entry: | No swears words founds to remove it! \`\`If you want to add some words type ${prefix}add-swear <SWEAR>\`\``);
+       if(Swears.length == 1) {
+           var to = 1;
+       }else if(Swears.length > 1) {
+           var to = `<1 to ${Swears.length}>`;// حقوق الفا كودز & عبود
+       }
+       if(!args[1]) return message.channel.send(`**➥ Useage:** ${prefix}remove-swear ${to}`);
+        if(isNaN(args[1])) return message.channel.send(`:no_entry: | The args must be a number!`);
+        if(args[1] > Swears.length) return message.channel.send(`:no_entry: | Please choose number from 1 to ${Swears.length}`);
+     
+       message.channel.send(`:white_check_mark: | Successfully remove **${Swears.splice(args[1] - 1, 1)}** from swears words`);
+       fs.writeFile('./swears.json', JSON.stringify(Swears), (err) => {
+           if(err) console.error(err);
+       })
+   }
+ 
+ 
+   if(command == prefix + 'remove-all-swears') {// حقوق الفا كودز & عبود
+       if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(':no_entry: | You dont have **ADMINISTRATOR** Permission!');
+       if(Swears.length < 1) return message.channel.send(`:no_entry: | No swears words founds to remove it! \`\`If you want to add some words type ${prefix}add-swear <SWEAR>\`\``);
+       message.channel.send(`:white_check_mark: | Successfully remove **${Swears.length}** Swears words!`);
+       Swears = [];
+       fs.writeFile('./swears.json', JSON.stringify(Swears), (err) => {
+           if(err) console.error(err);
+       })// حقوق الفا كودز & عبود
+   }
+});// حقوق الفا كودز & عبود
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    if(Swears.some(word => newMessage.content.toLowerCase().includes(word))) {// حقوق الفا كودز & عبود
+        if(oldMessage.member.hasPermission('ADMINISTRATOR')) return;
+        oldMessage.delete();// حقوق الفا كودز & عبود
+        oldMessage.channel.send(`:no_entry: | Hey <@${oldMessage.author.id}>! Dont swear or you will get mute!`).then(msg => msg.delete(2000));
+    }// حقوق الفا كودز & عبود
+});
 
 
 
@@ -1581,4 +1571,3 @@ client.on('messageDelete', message => {
 
 
 client.login(process.env.BOT_TOKEN);
-
