b951baf082a2aceedbdaed27fc03185a7be9fee3
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-30 10:28:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,9 +51,6 @@ client.on('message', message => {//help msg
 ❖${prefix}** cat ** ==>**لكي ترى صور قطط عشوائية**
 ❖${prefix}** roll ** ==>**لاختيار رقم عشوائي**
 By :<@427802430701436928> | __**شكرا لكم لاستعمال البوت**__
-
-`);
-
 }
 });
   if (message.content.startsWith('+bc')) {
@@ -67,7 +64,28 @@ message.guild.members.forEach(m => {
  
 });
   }
- 
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "مسح") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("```ضع عدد الرسائل التي تريد مسحها 👌```").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
 });
  client.login(process.env.BOT_TOKEN);
 
