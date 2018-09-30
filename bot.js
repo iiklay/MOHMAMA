dd3940e9e9f9e869004270cad7ab6c93b9543a38
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-30 10:53:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,69 @@ client.on('message', message => {
   if (message.content === 'ping') {
   }
  
+ client.on ('message', Sal => {
+    if (Sal.content === '+helps') {
+    var embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+    .setDescription(`
+      || جميع الاوامر في البوت || All Cmd In This Bot ||
+      اوامر العامه || +help-member
+      اوامر الاداره || +help-admin
+      اوامر اللعاب || +help-games
+      `)
+      Sal.channel.sendEmbed(embed)
+    }
+    });
  
+ client.on ('message', Sal => {
+      if (Sal.content === '+help-member') {
+      var embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+      .setDescription(`
+      || اوامر العامه || Cmd Member ||
+      +avatar l يعرضلك صورت حسابك
+      +server l يعرضلك معلموات عن السيرفر
+      +myrole l يعرضلك رتبتك
+      +roles l يعرضلك كل الروتبه في السيرفر
+      +bot l يعرضلك معلموت البوت
+      +id l يعرضلك معلموتك
+        `)
+        Sal.channel.sendEmbed(embed)
+      }
+      });
+ 
+ client.on ('message', Sal => {
+        if (Sal.content === '+help-admin') {
+        var embed = new Discord.RichEmbed()
+          .setColor('RANDOM')
+        .setDescription(`
+        || اوامر الادراه || Cmd Admin ||
+      +bc <message>  l لي ارسال رسالة لي كل الاعضاء
+      +ban <mention> l لاعطاء الشخص حضر
+      +kick <mention > l لاعطاء الشخص طرد
+      +clear l لمسح الشات
+      +mute <mention> l لاعطاء الشخص ميوت
+      +unmute <mention> 
+          `)
+          Sal.channel.sendEmbed(embed)
+        }
+        });
+ 
+ client.on ('message', Sal => {
+          if (Sal.content === '+help-games') {
+          var embed = new Discord.RichEmbed()
+            .setColor('RANDOM')
+          .setDescription(`
+          || اوامر اللعاب || Cmd Games ||
+         +لبدء لعبه مريم
+          -لو خيروك l لبدء لعبه لو خيروك
+          -صراحه l لبدء لعبه صراحه
+          -كت تويت l لبدء لعبه كت تويت
+          - اكس او l لبدء لعبه اكس او  
+            `)
+            Sal.channel.sendEmbed(embed)
+          }
+ });
   if (message.content.startsWith('+bc')) {
           if (!args[0]) {
 message.channel.send("**+bc <message>**");
@@ -55,5 +117,119 @@ client.on('message', msg => {
     }
 }
 });
- client.login(process.env.BOT_TOKEN);
+const zead = [
+   '*** انا اسمي مريم ***',
+   '*** مرحباَ ماهو اسمك ؟ ***',
+   `*** اهلا بك ! انا تائهه في هذا المكان  ***`,
+   '*** هل تود مساعدتي ؟ ***',
+   '*** لماذا هل انت قاسي القلب ؟ ***',
+   '*** انني اشفق عليك يجب ان تطهر روحك وتحب الخير للجميع ***',
+   '*** ابتعد عني قليل انني متعبة ***',
+	 '*** هل انت نادم على ماقلت ؟ ***',
+   '*** هل تود مساعدتي ؟ ***',
+   '*** واو اشكرك انك شخصاَ رائع ! ***',
+   '*** ابحث معي عن منزلي لقد كان قريباَ من هنا ***',
+   '*** ولاكن عندما حل الليل لم اعد ارى اي شيء ***',
+   '*** مذا تظن اين يوجد ؟ يمين او يسار ***',
+   '*** هيا اذاَ ***',
+   '*** اود ان اسئلك سؤال ونحن في الطريق ***',
+   '*** هل تراني فتاة لطيفة ام مخيفة ***',
+   '*** اشكرك !  ***',
+   '*** لقد وصلنا الى المنزل شكراَ جزيلَ انتطرني ثواني وسوف اعود ***',
+   '*** هل انت جاهز ؟ ***',
+   '*** لقد اخبرت والدي عنك وهم متحمسين لرؤيتك ***',
+   '*** هل تود ان تراهم الان ***',
+'*** انا لست الحوت الازرق كما يدعون ***',
+   '*** انا لست كاذبة صدقني***',
+   '*** لماذا ارى في عينيك الخوف ؟ ***',
+   '*** انا مجرد فتاة لطيفة تحب اللعب مع الجميع ***',
+   '*** اعرف كل شيء يحدث اسمع ذالك بالراديو ***',
+   '*** سمعت ان البشر يقتلون من اجل المال فقط ***',
+   '*** لماذا لم تدخل الغرفة ؟ ***',
+   '*** ههههههههههههههههههه انت الان مسجون في هذه الغرفة ***',
+   '*** لن تخرج حتى اعود لك بعد قليل ***',
+   '*** المفتاح معك ! اكتب .مريم  ***',
+   '*** مفتاح احمر , هل حصلت عليه ؟ ***',
+   '*** ان لم تحصل عليه , اكتب .مريم مرة اخرى ***',
+   '*** مفتاح اسود . هل حصلت عليه ؟ ***',
+   '*** اين تريد ان تختبئ بسرعة قبل ان تعود ***',
+   '*** لقد عادت من جديد الى المنزل ***',
+   '*** لا تصدر اي صوت ! ***',
+   '*** مريم : لقد عدت ***',
+   '*** مريم : يا ايها المخادع اين انت ***',
+   '*** مريم : اعلم انك هنا في المنزل ***',
+   '*** مريم : ماذا تريد ان تسمع ***',
+   '*** احد ما خرج من المنزل ***',
+   '*** انتظر الجزء الثاني عندما يوصل البوت 100 سيرفر , ساعدني في نشر البوت وادخل هذا السيرفر  ***'
+];
+ client.on('message', message => {
+ if (message.content.startsWith('+مريم')) {
+  var mariam= new Discord.RichEmbed()
+  .setTitle("لعبة مريم ..")
+  .setColor('RANDOM')
+  .setDescription(`${zead[Math.floor(Math.random() * zead.length)]}`)
+  .setImage("https://www.npa-ar.com/wp-content/uploads/2017/08/%D9%84%D8%B9%D8%A8%D8%A9-%D9%85%D8%B1%D9%8A%D9%85-300x200.jpg")
+   message.channel.sendEmbed(mariam);
+  }
+});
+client.on('message', async message =>{
+  if (message.author.boss) return;
+	var prefix = "+";
+
+if (!message.content.startsWith(prefix)) return;
+	let command = message.content.split(" ")[0];
+	 command = command.slice(prefix.length);
+	let args = message.content.split(" ").slice(1);
+	if (command == "mute") {
+		if (!message.channel.guild) return;
+		if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("انت لا تملك صلاحيات !! ").then(msg => msg.delete(5000));
+		if(!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.reply("البوت لايملك صلاحيات ").then(msg => msg.delete(5000));;
+		let user = message.mentions.users.first();
+		let muteRole = message.guild.roles.find("name", "Muted");
+		if (!muteRole) return message.reply("** لا يوجد رتبة الميوت 'Muted' **").then(msg => {msg.delete(5000)});
+		if (message.mentions.users.size < 1) return message.reply('** يجب عليك المنشن اولاً **').then(msg => {msg.delete(5000)});
+		let reason = message.content.split(" ").slice(2).join(" ");
+		message.guild.member(user).addRole(muteRole);
+		const muteembed = new Discord.RichEmbed()
+		.setColor("RANDOM")
+		.setAuthor(`Muted!`, user.displayAvatarURL)
+		.setThumbnail(user.displayAvatarURL)
+		.addField("**:busts_in_silhouette:  المستخدم**",  '**[ ' + `${user.tag}` + ' ]**',true)
+		.addField("**:hammer:  تم بواسطة **", '**[ ' + `${message.author.tag}` + ' ]**',true)
+		.addField("**:book:  السبب**", '**[ ' + `${reason}` + ' ]**',true)
+		.addField("User", user, true)
+		message.channel.send({embed : muteembed});
+		var muteembeddm = new Discord.RichEmbed()
+		.setAuthor(`Muted!`, user.displayAvatarURL)
+		.setDescription(`      
+${user} انت معاقب بميوت كتابي بسبب مخالفة القوانين
+${message.author.tag} تمت معاقبتك بواسطة
+[ ${reason} ] : السبب
+اذا كانت العقوبة عن طريق الخطأ تكلم مع المسؤلين
+`)
+		.setFooter(`في سيرفر : ${message.guild.name}`)
+		.setColor("RANDOM")
+	user.send( muteembeddm);
+  }
+if(command === `unmute`) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("**ليس لديك صلاحية لفك عن الشخص ميوت**:x: ").then(m => m.delete(5000));
+if(!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.reply("**ما عندي برمشن**").then(msg => msg.delete(6000))
+
+  let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!toMute) return message.channel.sendMessage("**عليك المنشن أولاّ**:x: ");
+
+  let role = message.guild.roles.find (r => r.name === "Muted");
+  
+  if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("**لم يتم اعطاء هذه شخص ميوت من الأساس**:x:")
+
+  await toMute.removeRole(role)
+  message.channel.sendMessage("**لقد تم فك الميوت عن شخص بنجاح**:white_check_mark:");
+
+  return;
+
+  }
+
+});
+مدري جربها
+client.login(process.env.BOT_TOKEN);
 
