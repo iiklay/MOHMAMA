ab453c3cc830a5b07c4a986a773f55d5fecb3921
iiklay <40916077+iiklay@users.noreply.github.com>
2019-03-24 10:37:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -755,7 +755,7 @@ if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'move')) {
  if (message.member.hasPermission("MOVE_MEMBERS")) {
  if (message.mentions.users.size === 0) {
- return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move @klay``")
+ return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move @khaled``")
 }
 if (message.member.voiceChannel != null) {
  if (message.mentions.members.first().voiceChannel != null) {
